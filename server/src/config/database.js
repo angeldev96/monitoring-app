4c892da057d78042d5793f4a64f6ea0ff41af463
Angel Valladares <arivalladares2.0@gmail.com>
2024-06-22 11:28:54 -0600

    update backend structure

---
+++ server/src/config/database.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false // set to console.log to see the raw SQL queries
+  }
+);
+
+
+
+module.exports = sequelize;
